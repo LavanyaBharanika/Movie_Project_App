a0a445d573c59553f6343edb04c2bbd53eda94c3
LavanyaBharanika <lavanyab1909@gmail.com>
2024-06-13 21:29:44 +0530

    Adjust Home component to handle guest mode

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,21 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import Sidebar from "./Sidebar";
 import Rows from "../Rows";
-import requests from '../request'
+import requests from '../request';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
-export default function Home({ auth }){
+export default function Home({ auth }) {
     const [user] = useAuthState(auth);
+    const isGuest = localStorage.getItem("isGuest") === "true";
 
-    return(
-        <div style={{display:'flex', paddingTop:'78px'}}>
-            
-            <div style={{marginLeft:'160px'}}>
-                <Rows title='Playing Now' fetchUrl={requests.NowPlaying}
-                    />
-                    <Rows title='Popular Movies'  fetchUrl={requests.PopularMovies}
-                    />
-                    <Rows title='Top Rated' fetchUrl={requests.TopRated} 
-                    />
-                    <Rows title='UpComing Movies' fetchUrl={requests.UpcomingMovies}
-                    />
+    return (
+        <div style={{ display: 'flex', paddingTop: '78px' }}>
+            <div style={{ marginLeft: '160px' }}>
+                <Rows title='Playing Now' fetchUrl={requests.NowPlaying} />
+                <Rows title='Popular Movies' fetchUrl={requests.PopularMovies} />
+                <Rows title='Top Rated' fetchUrl={requests.TopRated} />
+                <Rows title='Upcoming Movies' fetchUrl={requests.UpcomingMovies} />
             </div>
         </div>
-    )
+    );
 }
